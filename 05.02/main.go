d2cc78c58939a1d03aa54029a3adffde013fca10
Mario Carballo Zama <afk@ellugar.co>
2019-12-09 19:56:54 -0600

    Fixed modes and params having to be patched

--- 05.02/main.go
+++ 05.02/main.go
@@ -17,6 +17,12 @@ type opts struct {
 
 var _o opts
 
+func output(val int) {
+	fmt.Printf("\n┌─────────┐")
+	fmt.Printf("\n│%9d│", val)
+	fmt.Printf("\n└─────────┘\n\n")
+}
+
 func main() {
 	path := flag.String("path", "./input.txt", "input file path")
 	debug := flag.Bool("d", false, "debug text")
@@ -99,24 +105,49 @@ func getModes(instruction string, l int) []int {
 
 }
 
+// Some operations have the ourput at the end therefore the mode can't be inmediate mode  = 1
+func setParamsByOp(op int, arr, params, modes []int) []int {
+	l := len(params)
+	switch op {
+	case 1:
+		l--
+	case 2:
+		l--
+	case 3:
+		l--
+	case 4:
+		l = l
+	case 5:
+		l = l
+	case 6:
+		l = l
+	case 7:
+		l--
+	case 8:
+		l--
+	}
+
+	for i := range params {
+		if modes[i] == 0 && i < l {
+			params[i] = arr[params[i]]
+		}
+	}
+	return params
+}
+
 // Normally operations take the first two params to do something and the last to output the operation
 func getParams(op, pointer int, modes []int, arr []int) []int {
 	size := getSizeByOP(op)
-	result := make([]int, 0)
+	params := make([]int, 0)
 
 	for i := 0; i < size; i++ {
 		a := arr[pointer+i+1]
-		result = append(result, a)
+		params = append(params, a)
 	}
 
-	// Reference mode last param can't be reference
-	for i := range result {
-		if modes[i] == 0 && i < len(result)-1 {
-			result[i] = arr[result[i]]
-		}
-	}
+	params = setParamsByOp(op, arr, params, modes)
 
-	return result
+	return params
 }
 
 func getSizeByOP(op int) int {
@@ -129,31 +160,52 @@ func getSizeByOP(op int) int {
 		return 1
 	case 4:
 		return 1
+	case 5:
+		return 2
+	case 6:
+		return 2
+	case 7:
+		return 3
+	case 8:
+		return 3
 	}
 	return 0
 }
 
-func getJumpByOp(op int) int {
+func getJumpByOp(op int, pointer int, params, modes []int, arr []int) int {
 	switch op {
 	case 1:
-		return getSizeByOP(op) + 1
+		return getSizeByOP(op) + pointer + 1
 	case 2:
-		return getSizeByOP(op) + 1
+		return getSizeByOP(op) + pointer + 1
 	case 3:
-		return getSizeByOP(op) + 1
+		return getSizeByOP(op) + pointer + 1
 	case 4:
-		return getSizeByOP(op) + 1
+		return getSizeByOP(op) + pointer + 1
+		// Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+	case 5:
+		if params[0] != 0 {
+			return params[1]
+		}
+		return getSizeByOP(op) + pointer + 1
+		// Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+	case 6:
+		if params[0] == 0 {
+			return params[1]
+		}
+		return getSizeByOP(op) + pointer + 1
+		// Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+	case 7:
+		return getSizeByOP(op) + pointer + 1
+		// Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+	case 8:
+		return getSizeByOP(op) + pointer + 1
+
 	}
 	return 0
 }
 
-func output(val int) {
-	fmt.Printf("\n┌─────────┐")
-	fmt.Printf("\n│%9d│", val)
-	fmt.Printf("\n└─────────┘\n\n")
-}
-
-func operate(op, pointer int, params, arr []int) []int {
+func operate(op, pointer int, params, modes, arr []int) []int {
 	switch op {
 	case 1:
 		arr[params[2]] = params[0] + params[1]
@@ -165,8 +217,22 @@ func operate(op, pointer int, params, arr []int) []int {
 		arr[params[0]] = _o.input
 		break
 	case 4:
-		output(arr[params[0]])
+		output(params[0])
 		break
+		// Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+	case 7:
+		if params[0] < params[1] {
+			arr[params[2]] = 1
+		} else {
+			arr[params[2]] = 0
+		}
+		// Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+	case 8:
+		if params[0] == params[1] {
+			arr[params[2]] = 1
+		} else {
+			arr[params[2]] = 0
+		}
 	}
 	return arr
 }
@@ -183,88 +249,23 @@ func doOperation(arr []int, pointer int) []int {
 		return arr
 	}
 
-	if operation > 4 {
+	if operation > 8 {
+		fmt.Println("\nOperation not supported: ", operation)
+
+		// if _o.debug {
+		// 	fmt.Println("\nInput:\n", utils.FmtIntSlice(arr))
+		// }
 		return arr
 	}
 
 	modes := getModes(sInstruction, 3)
 	params := getParams(operation, pointer, modes, arr)
-	jump := getJumpByOp(operation)
+	jump := getJumpByOp(operation, pointer, params, modes, arr)
 
-	debugtxt += fmt.Sprintf("| modes: %v | params %v | jump %d", modes, params, jump)
+	debugtxt += fmt.Sprintf(" | modes: %v | params %v | jump %d", modes, params, jump)
 	logger.Debug(debugtxt)
-	// fmt.Println("operation", operation)
-	// fmt.Println("modes", modes)
-	// fmt.Println("params", params)
-	// fmt.Println("size", params)
 
-	arr = operate(operation, pointer, params, arr)
+	arr = operate(operation, pointer, params, modes, arr)
 
-	return doOperation(arr, pointer+jump)
+	return doOperation(arr, jump)
 }
-
-// func readOpcodes(input []int) []int {
-// 	logger.Debug("\n\n")
-// 	result := append([]int(nil), input...)
-// 	for i := 0; i < len(result); i++ {
-// 		instruction := strconv.Itoa(result[i])
-
-// 		val := 0
-// 		where := 0
-// 		operator := 0
-
-// 		a := result[i+1]
-// 		b := result[i+2]
-// 		c := result[i+3]
-
-// 		logger.Debug("[%2d] instruction: %05d - | a: m %4d - i %4d | b: m %4d - i %4d | c: m %4d - i %4d", i, z, modA, a, modB, b, modC, c)
-
-// 		switch operation {
-// 		case 1:
-// 			if modA == 0 {
-// 				a = result[a]
-// 			}
-
-// 			if modB == 0 {
-// 				b = result[b]
-// 			}
-
-// 			val = a + b
-// 			where = c
-// 			operator = 3
-// 			break
-// 		case 2:
-// 			if modA == 0 {
-// 				a = result[a]
-// 			}
-
-// 			if modB == 0 {
-// 				b = result[b]
-// 			}
-// 			val = a * b
-// 			where = c
-// 			operator = 3
-// 			break
-// 		case 3:
-// 			val = _o.input
-// 			where = a
-// 			operator = 1
-// 			break
-// 		case 4:
-// 			where = a
-// 			fmt.Printf("\n\n┌─────────┐")
-// 			fmt.Printf("\n│%9d│", result[where])
-// 			fmt.Printf("\n└─────────┘\n\n")
-// 			operator = 1
-// 			break
-// 		case 99:
-// 			return result
-// 			break
-// 		}
-// 		i += operator
-// 		result[where] = val
-// 		logger.Debug("[%2d] instruction: %05d - | a: m %4d - i %4d | b: m %4d - i %4d | c: m %4d - i %4d \n", i, z, modA, a, modB, b, modC, c)
-
-// 	}
-// 	return result
-// }
